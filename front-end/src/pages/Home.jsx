788dd1d0043c8ab1ce24f05dea8c3f18653bb536
nmtun <tungnlpb@gmail.com>
2025-05-16 03:43:19 +0700

    update

--- front-end/src/pages/Home.jsx
+++ front-end/src/pages/Home.jsx
@@ -1,22 +1,16 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import '../styles/Home.css';
+import Header from '../components/Header';
+import Navbar from '../components/Navbar';
 
 const Home = () => {
-  const navigate = useNavigate();
 
-  const handleLogout = () => {
-    localStorage.removeItem('token');
-    navigate('/login');
-  };
 
   return (
     <div className="home-container">
-      <h1>Trang Chủ</h1>
-      <p>Chào mừng bạn đã đăng nhập thành công!</p>
-      <button onClick={handleLogout} className="logout-button">
-        Đăng Xuất
-      </button>
+      <Header />
+      <Navbar />
     </div>
   );
 };
