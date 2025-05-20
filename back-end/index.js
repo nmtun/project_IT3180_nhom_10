b2bef036592ee38e16d4bdc58a7b1e6a764b295c
Gnuhq26 <gnuhq26@gmail.com>
2025-05-21 02:24:13 +0700

    Hungne

--- back-end/index.js
+++ back-end/index.js
@@ -62,7 +62,7 @@ app.use("/api/change", ChangeRoutes);
 (async () => {
   try {
     //await sequelize.sync(); // tạo bảng nếu chưa có
-    //await sequelize.sync({ force: true }); // xóa bảng và tạo lại - dùng khi cần làm mới cơ sở dữ liệu, sẽ bị mất dữ liệu
+    // wait sequelize.sync({ force: true }); // xóa bảng và tạo lại - dùng khi cần làm mới cơ sở dữ liệu, sẽ bị mất dữ liệu
     
     app.listen(PORT, () => {
       console.log(`Server is running at http://localhost:${PORT}`);
